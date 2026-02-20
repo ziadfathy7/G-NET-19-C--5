e2bab5cf536b5cf86b049e816b985723a2b49fa8
Ziad Fathy <66222287+ziadfathy7@users.noreply.github.com>
2026-02-20 20:25:47 +0200

    Question 1

---
+++ C#Basics05Assignment/Enums/Days.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Basics05Assignment.Enums
+{
+    enum Days
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+}
--- C#Basics05Assignment/Program.cs
+++ C#Basics05Assignment/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace C_Basics05Assignment
+﻿using C_Basics05Assignment.Enums;
+
+namespace C_Basics05Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+
+            //Part 1 Enums
+            /* Q1: Day of The Week Create an enum called DayOfWeek with values: Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday.
+                    Then write a program that:
+                    Asks the user to enter a day number (0–6).
+                    Converts it to the enum and prints the day name.
+                    Uses a switch statement to print whether it's a "Workday" or a "Weekend". */
+            //Days day;
+            //bool isParsed;
+            //do
+            //{
+            //    Console.WriteLine("Please Enter a Day Number from 0-6");
+            //    isParsed = Enum.TryParse(Console.ReadLine(), out day);
+            //} while(!isParsed || !Enum.IsDefined(day));
+            //Console.WriteLine(day);
+
+            //switch (day)
+            //{
+            //    case Days.Saturday:
+            //        Console.WriteLine("This day is Weekend");
+            //        break;
+            //    case Days.Sunday:
+            //        Console.WriteLine("This day is Workday");
+            //        break;
+            //    case Days.Monday:
+            //        Console.WriteLine("This day is Workday");
+            //        break;
+            //    case Days.Tuesday:
+            //        Console.WriteLine("This day is Workday");
+            //        break;
+            //    case Days.Wednesday:
+            //        Console.WriteLine("This day is Workday");
+            //        break;
+            //    case Days.Thursday:
+            //        Console.WriteLine("This day is Workday");
+            //        break;
+            //    case Days.Friday:
+            //        Console.WriteLine("This day is Weekend");
+            //        break;
+            //}
+            #endregion
+
+
         }
     }
 }
